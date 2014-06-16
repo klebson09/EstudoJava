9adcebca39073129895e34b95b8d216ecd645f04
klebson09 <silvaklebson_@hotmail.com>
2014-06-16 17:46:21 -0300

    Revert "Revert "Pegando data do sistema""
    This reverts commit 1709cd89579efcf22cfe5c3e0123d214a9f1658d.

--- EchoServer.java
+++ EchoServer.java
@@ -1,42 +1,51 @@
-import java.io.*; 
-import java.net.*; 
-import java.util.*; 
-public class EchoServer 
-{ 
-public static void main(String[] args ) 
-{ 
-try 
-{ 
-ServerSocket s = new ServerSocket(8189); 
-Socket incoming = s.accept( ); 
-try 
-{ 
-InputStream inStream = incoming.getInputStream(); 
-OutputStream outStream = incoming.getOutputStream(); 
+package Sd;
 
-Scanner in = new Scanner(inStream); 
-PrintWriter out = new PrintWriter(outStream, true /* autoFlush */); 
-out.println( "Hello! Enter BYE to exit." ); 
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-// echo client input 
-boolean done = false; 
-while (!done && in.hasNextLine()) 
-{ 
-String line = in.nextLine(); 
-System.out.println (line);
-out.println("Echo: " + line); 
-if (line.trim().equals("BYE")) 
-done = true; 
-} 
-} 
-finally 
-{ 
-incoming.close(); 
-} 
-} 
-catch (IOException e) 
-{ 
-e.printStackTrace(); 
-} 
-} 
-} 
+public class EchoServer {
+	public static Date data;
+	public static void main(String[] args) {
+		Scanner in;
+		OutputStream outStream; 
+		InputStream inStream;
+		Socket incoming ;
+		ServerSocket s;
+		try {
+			 s = new ServerSocket(8189);
+			 incoming = s.accept();
+			try {
+				 inStream = incoming.getInputStream();
+				 outStream = incoming.getOutputStream();
+
+				in = new Scanner(inStream);
+				PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+				out.println("Hello! Enter BYE to exit.");
+
+				
+				data = new Date();
+
+				// echo client input
+				boolean done = false;
+				while (!done && in.hasNextLine()) {
+					String line = in.nextLine();
+					System.out.println(line);
+					if (line.equals("data")) {
+						out.println(data);
+
+					}
+
+					out.println("Echo: " + line);
+					if (line.trim().equals("BYE"))
+						done = true;
+				}
+			} finally {
+				incoming.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
